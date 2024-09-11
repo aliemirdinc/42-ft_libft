6afbc4e74f5625b8a34af3fa3280970af2ce3287
aliemirdinc <aliemirdinc2@gmail.com>
2024-09-11 11:30:26 +0300

    version 1.9

---
+++ ft_strlcat.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   strlcat.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/11 10:10:42 by marvin            #+#    #+#             */
+/*   Updated: 2024/09/11 10:10:42 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	j;
+	size_t	dlen;
+	size_t	slen;
+
+	i = 0;
+	dlen = ft_strlen(dest);
+	slen = ft_strlen(src);
+	j = dlen;
+	if (size == 0 || size <= dlen)
+		return (slen + size);
+	while (src[i] != '\0' && i < size - dlen - 1)
+	{
+		dest[j] = src[i];
+		i++;
+		j++;
+	}
+	dest[j] = '\0';
+	return (dlen + slen);
+}
--- libft.h
+++ libft.h
@@ -27,5 +27,7 @@ void	*ft_memset(void *ptr, int value, size_t num);
 size_t	ft_strlen(const char *s);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
+
 
 #endif
