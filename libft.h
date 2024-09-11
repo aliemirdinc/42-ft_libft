30320b9cf537af3af7c8680bd37551aa4e9f133d
aliemirdinc <aliemirdinc2@gmail.com>
2024-09-11 11:31:34 +0300

    version 1.10

--- libft.h
+++ libft.h
@@ -29,5 +29,4 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 
-
 #endif
