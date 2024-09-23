a25b386b2a972d8a65d3654fa57086bda7639036
aliemirdinc <aliemirdinc2@gmail.com>
2024-09-23 12:45:24 +0300

    version 1.21

--- main.c
+++ main.c
@@ -1,19 +1,44 @@
 // main.c
-#include "libft.h"
 #include <stdio.h>
+#include "libft.h"  // ft_strjoin fonksiyonunun tanımlı olduğu header dosyanız
 
-#include <string.h> 
 
+// Burada, ft_strdup ve ft_substr fonksiyonlarının çalıştığını varsayıyorum.
+// Bunları kendi projendeki şekilde tanımlaman gerektiğini unutma.
 
-int	main(void)
+int main(void)
 {
-	const char *original = "Sizdebizimolanlardanmisiniz";
-	char *result;
+    char *s1 = "!!hello!!";
+    char *set = "!";
+    char *result;
 
-    result = ft_substr(original, 5, 22);
-    printf("Kopyalanmis sonucumuz = %s\n", result);
+    result = ft_strtrim(s1, set);
+    printf("Input: \"%s\"\nSet: \"%s\"\nResult: \"%s\"\n", s1, set, result);
+    free(result);
 
-	free(result);
+    s1 = "   42 school   ";
+    set = " ";
+    result = ft_strtrim(s1, set);
+    printf("Input: \"%s\"\nSet: \"%s\"\nResult: \"%s\"\n", s1, set, result);
+    free(result);
 
-	return (0);
+    s1 = "abcdef";
+    set = "xyz";
+    result = ft_strtrim(s1, set);
+    printf("Input: \"%s\"\nSet: \"%s\"\nResult: \"%s\"\n", s1, set, result);
+    free(result);
+
+    s1 = "xxxxxyhello worldyyxxxxx";
+    set = "yx";
+    result = ft_strtrim(s1, set);
+    printf("Input: \"%s\"\nSet: \"%s\"\nResult: \"%s\"\n", s1, set, result);
+    free(result);
+
+    s1 = "";
+    set = "!";
+    result = ft_strtrim(s1, set);
+    printf("Input: \"%s\"\nSet: \"%s\"\nResult: \"%s\"\n", s1, set, result);
+    free(result);
+
+    return 0;
 }
