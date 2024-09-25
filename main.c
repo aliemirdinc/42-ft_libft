344ded342600457ecb62c9b16d0daf05af94a16c
aliemirdinc <aliemirdinc2@gmail.com>
2024-09-25 21:03:30 +0300

    version 1.26

--- main.c
+++ main.c
@@ -1,39 +1,14 @@
-#include "libft.h"
-#include <stdio.h>
-#include <stdlib.h> // malloc ve free için gerekli
-
-// Her karakteri büyük harfe çeviren fonksiyon
-void toupper_function(unsigned int i, char *c)
-{
-    (void)i; // i kullanılmadığı için bu satırı ekliyoruz
-    if (*c >= 'a' && *c <= 'z')
-        *c -= 32; // Küçük harfi büyük harfe çevirir
-}
+#include "libft.h"  // ft_putchar_fd fonksiyonu için gerekli
+#include <stdio.h>  // printf ve perror için gerekli
 
 int main(void)
 {
-    // Bellekten dinamik olarak yer ayırıp bir string oluşturuyoruz
-    char *str = (char *)malloc(sizeof(char) * 14); // "hello world!" ve son null karakter için 14 karakterlik yer
-    if (!str)
-    {
-        printf("Bellek ayırma hatası!\n");
-        return (1);
-    }
+	printf("Ekrana yazdirma testi: \n");
+	ft_putstr_fd("selamlar", 1); // 'A' harfini ekrana yazdır
+	printf("\n"); // Satır sonu ekleyelim ki çıktılar düzgün olsun
 
-    // Stringimizi başlatıyoruz
-    str = ft_memcpy(str, "hello world!", 14); // "hello world!" stringini str'e kopyalıyoruz
-
-    // Orijinal stringi yazdırıyoruz
-    printf("Original String: %s\n", str);
-
-    // Stringin her karakterini büyük harfe çeviriyoruz
-    ft_striteri(str, toupper_function);
-
-    // Büyük harfe çevrilen stringi yazdırıyoruz
-    printf("Uppercase String: %s\n", str);
-
-    // Belleği temizliyoruz
-    free(str);
-
-    return 0;
+	// 2. Hata çıkışına yazdırma (stderr)
+	printf("Hata çikişina yazdirma testi: \n");
+	ft_putstr_fd("bilmem", 2); // 'B' harfini hata çıkışına yazdır
+	printf("\n");
 }
