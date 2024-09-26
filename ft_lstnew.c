cecd66fd1e7d308b672b2ea8ae16476ad01d19bf
aliemirdinc <aliemirdinc2@gmail.com>
2024-09-26 12:58:00 +0300

    version 2.0

---
+++ ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/26 10:44:50 by marvin            #+#    #+#             */
+/*   Updated: 2024/09/26 10:44:50 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*ptr;
+
+	ptr = (t_list *)malloc(sizeof(t_list));
+	if (!ptr)
+		return (NULL);
+	ptr -> content = content;
+	ptr -> next = (NULL);
+	return (ptr);
+}
--- main.c
+++ main.c
@@ -1,26 +1,29 @@
-#include "libft.h"  // ft_putchar_fd fonksiyonu için gerekli
-#include <stdio.h>  // printf ve perror için gerekli
+#include <stdio.h>   // printf için
+#include <stdlib.h>  // malloc için
+#include "libft.h"   // ft_lstnew için
+
 
 int main(void)
 {
-	ft_putnbr_fd(0, 1);
-	ft_putchar_fd('\n', 1);
+    // Test için örnek bir içerik oluşturuyoruz
+    char *content = "Merhaba Dünya";
 
-	ft_putnbr_fd(5, 1);
-	ft_putchar_fd('\n', 1);
+    // Yeni bir liste elemanı oluşturuyoruz
+    t_list *new_element = ft_lstnew((void *)content);
 
-	ft_putnbr_fd(1236, 1);
-	ft_putchar_fd('\n', 1);
+    // Bellekten yer ayırma işlemi başarısız olursa
+    if (new_element == NULL)
+    {
+        printf("Bellekten yer ayırma başarısız oldu.\n");
+        return (1);  // Programdan çık
+    }
 
-	ft_putnbr_fd(-5, 1);
-	ft_putchar_fd('\n', 1);
+    // Yeni elemanın content kısmını ve next kısmını kontrol ediyoruz
+    printf("Content: %s\n", (char *)new_element->content);
+    printf("Next pointer: %p\n", new_element->next);
 
-	ft_putnbr_fd(-235, 1);
-	ft_putchar_fd('\n', 1);
+    // Bellekten ayırdığımız alanı boşaltıyoruz
+    free(new_element);
 
-	ft_putnbr_fd(-10, 1);
-	ft_putchar_fd('\n', 1);
-
-	ft_putnbr_fd(-2147483648, 1);
-	ft_putchar_fd('\n', 1);
+    return 0;
 }
